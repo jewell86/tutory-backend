1aca41c847bff37e14ced4f87d11705108b1538d
Nate <ndsunc@gmail.com>
2018-08-07 14:56:09 -0700

    Removed tutorials_contents table from db - movied templates_id into contents table, movied img into tutorials table

--- db/migrations/20180807141207_contents.js
+++ db/migrations/20180807141207_contents.js
@@ -2,8 +2,9 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('contents', table => {
     table.increments().notNullable()
+    table.integer('tutorials_id').notNullable()
+    table.foreign('tutorials_id').references('tutorials.id').onDelete('CASCADE')
     table.string('url', 255).notNullable()
-    table.string('img', 255)
   })
 };
 
--- db/seeds/08-contents.js
+++ db/seeds/08-contents.js
@@ -2,14 +2,14 @@ console.log('Seeding Tutorial Content')
 
 exports.seed = function(knex, Promise) {
   return knex('contents').insert([
-    { id: 1, url: "https://www.youtube.com/watch?v=OMOVFvcNfvE", img: "https://images-na.ssl-images-amazon.com/images/I/814X0lSCJQL._SL1412_.jpg" },
-    { id: 2, url: "https://www.youtube.com/watch?v=fIHH5-HVS9o" },
-    { id: 3, url: "https://www.youtube.com/watch?v=WI3hecGO_04" },
-    { id: 4, url: "https://www.youtube.com/watch?v=j1p0_R8ZLB0" },
-    { id: 5, url: "https://www.youtube.com/watch?v=CQERFnGvi_A", img: "https://upload.wikimedia.org/wikipedia/en/7/73/Fifty_Shades_of_Grey_poster.jpg" },
-    { id: 6, url: "https://www.youtube.com/watch?v=NBuNDeFjDUY", img: "https://images-na.ssl-images-amazon.com/images/I/518oHwOKn2L._SY445_.jpg" },
-    { id: 7, url: "https://www.youtube.com/watch?v=gtTfd6tISfw", img: "https://m.media-amazon.com/images/M/MV5BYzE5MjY1ZDgtMTkyNC00MTMyLThhMjAtZGI5OTE1NzFlZGJjXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_.jpg" },
-    { id: 8, url: "https://www.youtube.com/watch?v=AuzyODgWRp4", }
+    { id: 1, tutorials_id: 1, url: "https://www.youtube.com/watch?v=OMOVFvcNfvE" },
+    { id: 2, tutorials_id: 1, url: "https://www.youtube.com/watch?v=fIHH5-HVS9o" },
+    { id: 3, tutorials_id: 1, url: "https://www.youtube.com/watch?v=WI3hecGO_04" },
+    { id: 4, tutorials_id: 1, url: "https://www.youtube.com/watch?v=j1p0_R8ZLB0" },
+    { id: 5, tutorials_id: 2, url: "https://www.youtube.com/watch?v=CQERFnGvi_A" },
+    { id: 6, tutorials_id: 3, url: "https://www.youtube.com/watch?v=NBuNDeFjDUY" },
+    { id: 7, tutorials_id: 4, url: "https://www.youtube.com/watch?v=gtTfd6tISfw" },
+    { id: 8, tutorials_id: 4, url: "https://www.youtube.com/watch?v=AuzyODgWRp4" }
   ])
   .then(() => {
     return knex.raw(`SELECT setval('contents_id_seq', (SELECT MAX(id) FROM contents))`)
