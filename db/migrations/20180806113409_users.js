e968e5509e8212db47f996bd23f4fa068ea1c7c1
Nate <ndsunc@gmail.com>
2018-08-07 09:51:12 -0700

    Users have first names, last names, photos, public/private boolean attributes added to table

--- db/migrations/20180806113409_users.js
+++ db/migrations/20180806113409_users.js
@@ -5,7 +5,12 @@ exports.up = function(knex, Promise) {
     table.string('username', 50).notNullable()
     table.string('email', 75).notNullable()
     table.string('password', 75).notNullable()
-    
+    table.string('photo_url', 255)
+    table.string('first_name', 75)
+    table.string('last_name', 75)
+    table.text('about_me')
+    table.boolean('public').defaultTo(true)
+
     table.unique('username')
     table.unique('email')
     table.timestamps(true, true)
