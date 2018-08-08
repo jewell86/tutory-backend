a8c6ba02bd750064548db7db022ae5e103850d21
Branden <b.lowe333@gmail.com>
2018-08-08 15:20:42 -0700

    update knexfile.js to support heroku deployment

--- knexfile.js
+++ knexfile.js
@@ -1,16 +1,33 @@
 const path = require('path')
 require('dotenv').load()
 
-module.exports = {
-  test: { client: 'pg', connection: process.env.DATABASE_URL },
-  development: {
-    client: 'pg',
-    connection: process.env.DATABASE_URL,
-    migrations: {
-      directory: path.join(__dirname, 'db', 'migrations')
-    },
-    seeds: {
-      directory: path.join(__dirname, 'db', 'seeds')
-    }
+const config = {
+  client: 'pg',
+  connection: process.env.DATABASE_URL,
+  migrations: {
+    directory: path.join(__dirname, 'src', 'db', 'migrations')
+  },
+  seeds: {
+    directory: path.join(__dirname, 'src', 'db', 'seeds')
   }
 }
+
+module.exports = {
+  test: config,
+  production: config,
+  development: config
+}
+
+// module.exports = {
+//   test: { client: 'pg', connection: process.env.DATABASE_URL },
+//   development: {
+//     client: 'pg',
+//     connection: process.env.DATABASE_URL,
+//     migrations: {
+//       directory: path.join(__dirname, 'db', 'migrations')
+//     },
+//     seeds: {
+//       directory: path.join(__dirname, 'db', 'seeds')
+//     }
+//   }
+// }
