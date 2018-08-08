5988d34f6392b4f954fa972bfed339e73e961518
Branden <b.lowe333@gmail.com>
2018-08-08 15:36:53 -0700

    code cleanup

--- knexfile.js
+++ knexfile.js
@@ -13,21 +13,6 @@ const config = {
 }
 
 module.exports = {
-  test: config,
   production: config,
   development: config
 }
-
-// module.exports = {
-//   test: { client: 'pg', connection: process.env.DATABASE_URL },
-//   development: {
-//     client: 'pg',
-//     connection: process.env.DATABASE_URL,
-//     migrations: {
-//       directory: path.join(__dirname, 'db', 'migrations')
-//     },
-//     seeds: {
-//       directory: path.join(__dirname, 'db', 'seeds')
-//     }
-//   }
-// }
